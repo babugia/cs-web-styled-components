e7623a08ce862568122258924a47933124ae2cde
Guilherme Babugia <babugia@Guilhermes-MacBook-Pro.local>
2020-03-08 16:31:23 -0300

    add thunk middleware

--- src/Matches/Matches.jsx
+++ src/Matches/Matches.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import allActions from '../store/actions';
+import { setMenuSelectedDispatcher } from '../Common/actionsDispatcher';
 import { menuItemType } from '../Header/headerUtils';
 
 const Matches = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(allActions.setMenuSelected(menuItemType.MATCHES));
+    // dispatch(allActions.setMenuSelected(menuItemType.MATCHES));
+    dispatch(setMenuSelectedDispatcher(menuItemType.MATCHES));
   }, [dispatch]);
 
   return <div>Matches</div>;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import rootReducer from './store/reducers';
 import './index.css';
 import App from './App';
 
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+// eslint-disable-next-line no-undef
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
+// const store = createStore(
+//   rootReducer,
+//   applyMiddleware(thunk),
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
 
 ReactDOM.render(
   <Provider store={store}>
@@ -17,3 +23,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+// TODO: create configureStore file
