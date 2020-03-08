2ae348ec2854b34be35f48050539331afc108a28
Guilherme Babugia <babugia@Guilhermes-MacBook-Pro.local>
2020-03-08 18:34:48 -0300

    setMenuSelectedDispatcher in Teams component

--- src/Teams/Teams.jsx
+++ src/Teams/Teams.jsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setMenuSelectedDispatcher } from '../Common/actionsDispatcher';
+import { menuItemType } from '../Header/headerUtils';
 
-const Teams = () => <div>Teams</div>;
+const Teams = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setMenuSelectedDispatcher(menuItemType.TEAMS));
+  }, [dispatch]);
+
+  return <div>Teams</div>;
+};
 
 export default Teams;
